7a7e706a06497a5929d95c8f159a4b5cebf1838c
Ahmad Aslam <ahmadaslam664@gmail.com>
2022-08-17 17:10:06 +0500

    seeder changing

--- database/seeders/GameNameSeeder.php
+++ database/seeders/GameNameSeeder.php
@@ -17,6 +17,21 @@ class GameNameSeeder extends Seeder
     public function run()
     {
         // $this->call(GameNameFactory::class);
-        GameName::factory()->count(10)->create();
+        $names = array(
+            array('name' => 'Canoeing'), 
+            array('name' => 'Morning Runs'),
+            array('name' => 'Track Runs'),
+            array('name' => 'Frisbee'),
+            array('name' => 'Lead Cycling'),
+            array('name' => 'Lead Walks'),
+            array('name' => 'Cooking'),
+            array('name' => 'Gardening'),
+            array('name' => 'Pickelball'),
+        );
+        foreach ($names as $name) {
+            GameName::create([
+             'name' => $name['name'],
+           ]);
+        }
     }
 }
