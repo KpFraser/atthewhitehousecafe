f1929ea8c5c15e7d780c07966b4259dec716aca3
Salman Ifrahim <salman9607@gamil.com>
2022-08-17 12:46:07 +0500

    fix_footer, routes

--- resources/js/Components/MasterFooter.vue
+++ resources/js/Components/MasterFooter.vue
@@ -1,5 +1,6 @@
 <script setup>
-    const props = defineProps({
+import { Link } from '@inertiajs/inertia-vue3';
+const props = defineProps({
         footerLists: {
             Type: Object,
             required: true
@@ -10,8 +11,10 @@
     <footer class="fixed w-full bottom-0">
         <div class="grid mx-auto text-[13.5px] sm:text-[20px] justify-center max-w-lg bg-white py-4 border border-black grid-cols-4">
             <div class="text-center text-[#639f1e] hover:text-green-600" v-for="footer in footerLists">
-                <i :class="`mx-auto fa-2x ${footer.class}`"></i>
+                <Link :href="route(footer.to)">
+                    <i :class="`mx-auto fa-2x ${footer.class}`"></i>
                 <div class="text-center">{{ footer.label }}</div>
+                </Link>
             </div>
         </div>
     </footer>
--- resources/use/useFooterList.js
+++ resources/use/useFooterList.js
@@ -1,9 +1,9 @@
 export default function useFooterList () {
     const footerLists = [
-        {label: 'surveys', class: 'far fa-file-invoice'},
-        {label: 'projects', class: 'fal fa-lightbulb-on'},
-        {label: 'relationships', class: 'fal fa-user-friends'},
-        {label: 'accounts', class: 'fas fa-user-circle'},
+        {label: 'surveys', class: 'far fa-file-invoice', to: 'survey'},
+        {label: 'projects', class: 'fal fa-lightbulb-on', to: 'project'},
+        {label: 'relationships', class: 'fal fa-user-friends', to: 'dashboard'},
+        {label: 'accounts', class: 'fas fa-user-circle', to: 'dashboard'},
     ]
     return { footerLists }
 }
