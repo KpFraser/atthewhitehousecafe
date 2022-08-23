4d8622f059b9f0e247b3a4e8403c69aa74e4d94b
Ahmad Aslam <ahmadaslam664@gmail.com>
2022-08-23 12:37:58 +0500

    some changes

--- resources/js/Pages/survey/diet.vue
+++ resources/js/Pages/survey/diet.vue
@@ -1,18 +1,12 @@
 <script setup>
     import BreezeAuthenticatedLayout from '@/Layouts/Authenticated.vue';
-    import BreezeLabel from '@/Components/Label.vue';
-    import BreezeInput from '@/Components/Input.vue';
     import MasterFooter from '@/Components/MasterFooter.vue';
     import MasterHeader from '@/Components/MasterHeader.vue';
-    import BreezeButton from '@/Components/Button.vue';
-    import commonFunctions from "@/use/common";
-    import { Head, Link } from '@inertiajs/inertia-vue3';
-    import { ref, reactive , onMounted} from 'vue';
+    import { ref , onMounted} from 'vue';
     import axios from 'axios';
     import useFooterList from "../../../use/useFooterList";
 
-    const { Toast } = commonFunctions()
-    const { footerLists , avb } = useFooterList()
+    const { footerLists  } = useFooterList()
     const names = ref({})
 
     const surveyProjects = () =>{
@@ -21,6 +15,14 @@
             .then((response)=>{
                 names.value = response.data
             })
+    }
+
+    const nameEdit = (id) =>{
+        axios
+            .post('/name-edit', id)
+            .then((response)=>{
+                console.log(response)
+        });
     }
     onMounted( ()=> {
     surveyProjects ()
@@ -37,11 +39,17 @@
                         <h1 class="text-3xl font-sans text-center bg-opacity-75 p-3">PROPOSED</h1>
                         <div class="text-center">Diet of Activities</div>
                     </div>
-                    <div class="flex ml-5 mb-5 items-center" v-for="name in names">
-                        <i class="far fa-pencil text-[16px]"></i>
-                        <div class="ml-5 text-[16px]">{{name.name}}</div>
+                    <div class="flex ml-5 mb-5" v-for="name in names">
+                        <div class="flex items-center ml-5 mb-5">
+                            <i class="far fa-pencil text-[16px] hover:text-[#639f1e]"></i>
+                            <div class="ml-5 text-[16px]">{{name.name}}</div>
+                        </div>
+                        <div class="flex ml-5 items-center mb-5">
+                            <i class="far fa-check text-[20px] hover:text-[#639f1e]" @click="nameEdit(name.id)"></i>
+                            <input type="text" v-model="name.name" class="ml-4 h-8 rounded">
+                        </div>
                     </div>
-                    
+
                 </div>
             </div>
             <MasterFooter
