4f95c3da893429fd48d332987cdd6b3f097c4f29
Ahmad Aslam <ahmadaslam664@gmail.com>
2022-09-13 12:43:46 +0500

    ticket completed

--- resources/js/Components/MasterHeader.vue
+++ resources/js/Components/MasterHeader.vue
@@ -6,10 +6,8 @@ import { Link } from '@inertiajs/inertia-vue3';
 <template>
 <div class="mx-auto max-w-lg rounded-br-[6.5rem] bg-[#20351d] font-serif py-20 bg-opacity-75 max-h-28 flex items-center justify-between">
     <div class="text-white ml-10">
-        <div class="text-[25px]">{{ $page.props.auth.user?.name }}</div>
-        <div class="text-[25px]" v-if="$page.props.auth.user === null">Vistor</div>
-        <div class="text-[16px]">{{ $page.props.auth.user?.email }}</div>
-        <div class="text-[16px]" v-if="$page.props.auth.user === null">none@e-mail.com</div>
+        <div class="text-[25px]">{{ $page.props.auth.user === null ? "vistor": $page.props.auth.user?.name}}</div>
+        <div class="text-[16px]">{{ $page.props.auth.user === null ? "none@e-mail.com": $page.props.auth.user?.email }}</div>
         <Link :href="route('logout')" class="font-medium text-white hover:text-[#639f1e] !0px-0" method="post" as="button">
             Log Out
         </Link>
