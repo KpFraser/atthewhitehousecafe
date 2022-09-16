16a40fad19cba8f833b2db5891a424c40c1244c5
Ahmad Aslam <ahmadaslam664@gmail.com>
2022-09-16 16:27:03 +0500

    tickets completed

--- resources/js/Pages/project/allProjects.vue
+++ resources/js/Pages/project/allProjects.vue
@@ -82,11 +82,11 @@ onMounted( ()=> {
                   <div class="text-black border-4 border-b-4 border-[#20351d] mx-10 border-opacity-75 mb-28 space-y-4 bg-white text-lg">
                         <ul class="nav w-full nav-tabs flex" id="tabs-tab" role="tablist">
                             <li class="nav-item w-1/2" role="presentation">
-                                <a href="#tabs-home" @click="(isActive = 1)" :class="{'bg-[#20351d] !text-white': isActive === 1 }" class="text-center border-opacity-75 border-[#20351d] bg-opacity-75 focus:bg-opacity-75 focus:text-white block font-bold text-xs focus:bg-[#20351d] text-[#20351d] uppercase border-b-2 border-r-2 border-transparent px-6 py-3 hover:border-transparent hover:bg-gray-100 active " id="tabs-home-tab" data-bs-toggle="pill" data-bs-target="#tabs-home" role="tab" aria-controls="tabs-home"
+                                <a href="#tabs-home" @click="(isActive = 1)" :class="{'bg-[#20351d] !text-white': isActive === 1 }" class="text-center border-opacity-75 border-[#20351d] bg-opacity-75 focus:bg-opacity-75 focus:text-white block font-bold text-xs focus:bg-[#20351d] text-[#20351d] uppercase border-b-2 border-r-2 border-transparent px-6 py-3 active" id="tabs-home-tab" data-bs-toggle="pill" data-bs-target="#tabs-home" role="tab" aria-controls="tabs-home"
                                    aria-selected="true">All</a>
                             </li>
                             <li class="nav-item w-1/2" role="presentation">
-                                <a href="#tabs-profile" @click="(isActive = 2)" :class="{'bg-[#20351d] !text-white': isActive === 2 }" class="text-center border-opacity-75 border-[#20351d] bg-opacity-75 focus:bg-opacity-75 focus:text-white block font-bold text-xs  focus:bg-[#20351d] text-[#20351d] uppercase border-b-2 border-transparent px-6 py-3 hover:border-transparent hover:bg-gray-100 " id="tabs-profile-tab" data-bs-toggle="pill" data-bs-target="#tabs-profile" role="tab"
+                                <a href="#tabs-profile" @click="(isActive = 2)" :class="{'bg-[#20351d] !text-white': isActive === 2 }" class="text-center border-opacity-75 border-[#20351d] bg-opacity-75 focus:bg-opacity-75 focus:text-white block font-bold text-xs  focus:bg-[#20351d] text-[#20351d] uppercase border-b-2 border-transparent px-6 py-3 " id="tabs-profile-tab" data-bs-toggle="pill" data-bs-target="#tabs-profile" role="tab"
                                    aria-controls="tabs-profile" aria-selected="false">User</a>
                             </li>
                         </ul>
